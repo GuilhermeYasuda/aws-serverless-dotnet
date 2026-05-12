80f1b4b5f1b6aa31028c16217ddd4650d777f518
Guilherme Harunobu Yasuda <haru_yasuda@hotmail.com>
2026-05-12 01:01:40 -0300

    feat: adicionar classe Aluno com mapeamento para DynamoDB

---
+++ AlunoLambda/Aluno.cs
@@ -0,0 +1,20 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlunoLambda
+{
+    [DynamoDBTable("alunos")]
+    public class Aluno
+    {
+        [DynamoDBHashKey("id")]
+        public int? Id { get; set; }
+        [DynamoDBProperty("nome")]
+        public string? Nome { get; set; }
+        [DynamoDBProperty("sobrenome")]
+        public string? Sobrenome { get; set; }
+        [DynamoDBProperty("classe")]
+        public int? Classe { get; set; }
+    }
+}
